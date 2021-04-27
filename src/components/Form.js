e89f4a53388b4f8a8905388e0458d27f551ed083
TheNootLinja <thenootlinja@gmail.com>
2021-04-26 21:50:10 -0400

    Add select field for service rendered styles for it

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,25 +6,81 @@ import Input from './Input';
 
 
 function Form() {
+    // TODO: Change over state below to using a reducer so that we can
+    // TODO: have a single state and hook so we don't have to have a
+    // TODO: separate set function for each.
     const [fName, setFName] = useState('');
     const [lName, setLName] = useState('');
     const [email, setEmail] = useState('');
     const [comment, setComment] = useState('');
-    const handleClick = () => {
-        console.log('CLICKED!')
+    const [service, setService] = useState('');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const formStateObject = {
+            fName,
+            lName,
+            email,
+            service,
+            comment,
+        }
+        console.log(formStateObject);
     }
-    const handleChange = (e) => {
+    const fNameChange = (e) => {
+        setFName(e.target.value);
+    };
+    const lNameChange = (e) => {
+        setLName(e.target.value);
+    };
+    const emailChange = (e) => {
+        setEmail(e.target.value);
+    };
+    const serviceChange  = (e) => {
+        setService(e.target.value);
+    };
+    const commentChange = (e) => {
         setComment(e.target.value);
-        console.log(comment);
     };
     return (
         <form>
-            <Input name='fname' label='First Name' type='text' phText='Whats your first name?'/>
-            <Input name='lname' label='Last Name' type='text' phText='Whats your last name?'/>
-            <Input name='email' label='Email' type='text' phText='Whats your first email?'/>
-            <Input name='comment' label='Comment' type='textarea' phText='What did you think of the service?' changeHandler={handleChange}/>
-            <Button btext='submit' clickHandler={handleClick}/>
-            <p>{comment}</p>
+            <Input 
+            name='fname' 
+            label='First Name' 
+            type='text' 
+            phText='Whats your first name?'
+            changeHandler={fNameChange}
+            />
+            <Input 
+            name='lname' 
+            label='Last Name' 
+            type='text' 
+            phText='Whats your last name?'
+            changeHandler={lNameChange}
+            />
+            <Input 
+            name='email' 
+            label='Email' 
+            type='text' 
+            phText='Whats your first email?'
+            changeHandler={emailChange}
+            />
+            <label htmlFor="service-used">Service Used</label>
+            <select name="service-used" id="dropdown" onChange={serviceChange}>
+                <option value="New Vehicle Sale">New Vehicle Sale</option>
+                <option value="Used Vehicle Sale">Used Vehicle Sale</option>
+                <option value="Vehicle Trade-In">Vehicle Trade-In</option>
+                <option value="Vehicle Refinance">Vehicle Refinance</option>
+            </select>
+
+            <Input 
+            name='comment' 
+            label='Comment' 
+            type='textarea' 
+            phText='What did you think of the service?' 
+            changeHandler={commentChange}
+            />
+            <Button btext='submit' clickHandler={handleSubmit}/>
+            <p></p>
+            <p></p>
         </form>
     )
 }
--- src/components/Input.js
+++ src/components/Input.js
@@ -2,7 +2,7 @@ function Input({name, label, type, phText, changeHandler}) {
     return (
         <div className="input-group">
             <label htmlFor={name}>{label}</label>
-            {type === "text" ? <input name={name} type={type} placeholder={phText}/> : <textarea placeholder={phText} onChange={changeHandler}></textarea>}
+            {type === "text" ? <input name={name} type={type} placeholder={phText} onChange={changeHandler}/> : <textarea placeholder={phText} onChange={changeHandler}></textarea>}
         </div>
     )
 }
